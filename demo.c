9ff10a74721d18111987ca03b5362ad71205ce9f
gyw <110@qq.com>
2025-12-23 17:01:58 +0800

    haha

--- demo.c
+++ demo.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+   struct Student{
+    char name[100];
+    char personality[100];
+    struct Student *next;
+   };
 int main(){
-    int j,c;
-    char n[2][5]={"8980","9198"};
-    for(j=3; j>=0; j--){
-        c = n[0][j] + n[1][j] - 2 * '0';
-        n[0][j]=c % 10 + '0';
-    }
-    for(j=0; j<=1; j++)
-    puts(n[j]);
+    struct Student a,b,c,*p,*head;
+    strcpy(a.name,"Milker");  strcpy(a.personality,"Gay");
+    strcpy(b.name,"Doubi_Leng"); strcpy(b.personality,"Good boy");
+    strcpy(c.name,"Lev"); strcpy(c.personality,"Good girl");
+    head = &a;
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+    p = head;
+    do{
+        printf("%s: %s\n",p->name,p->personality);
+        p=p->next;
+    }while(p!=NULL);
     return 0;
-}
+}                                                
